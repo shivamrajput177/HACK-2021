90ddc3e1ce8cb27ba3843ccae9dd796a25cf6e57
Prem <prem47645@gmail.com>
2021-01-31 19:03:27 +0530

    Initial setup for video page

--- frontend/src/pages/VideoView.js
+++ frontend/src/pages/VideoView.js
@@ -1,13 +1,61 @@
-import React from 'react' 
-import Navbar from '../components/Navbar'
+import React, { Fragment, useState } from "react";
+import Navbar from "../components/Navbar";
+import "../Resources/styles.css";
 
 const VideoView = () => {
-    return (
-        <div>
-            <Navbar />
-            Video View
+  const [playing, setPlaying] = useState(false);
+
+  const HEIGHT = 500;
+  const WIDTH = 500;
+  const startVideo = () => {
+    setPlaying(true);
+    navigator.getUserMedia(
+      {
+        video: true,
+      },
+      (stream) => {
+        let video = document.getElementsByClassName("liveVideoFeed")[0];
+        if (video) {
+          video.srcObject = stream;
+        }
+      },
+      (err) => console.error(err)
+    );
+  };
+
+  const stopVideo = () => {
+    setPlaying(false);
+    let video = document.getElementsByClassName("liveVideoFeed")[0];
+    video.srcObject.getTracks()[0].stop();
+  };
+
+  return (
+    <Fragment>
+      <Navbar />
+      <div className="videoView">
+        <div className="videoContainer">
+          <video
+            height={HEIGHT}
+            width={WIDTH}
+            muted
+            autoPlay
+            className="liveVideoFeed"
+          ></video>
         </div>
-    )
-}
+        <div className="btn">
+          {playing ? (
+            <button onClick={stopVideo} className="btn-danger">
+              Stop
+            </button>
+          ) : (
+            <button onClick={startVideo} className="btn-success">
+              Start
+            </button>
+          )}
+        </div>
+      </div>
+    </Fragment>
+  );
+};
 
 export default VideoView;
